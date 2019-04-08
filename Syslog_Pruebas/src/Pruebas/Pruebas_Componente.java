ba5d13cfe96cac60d580e977f636d702c193fa0b
asantamv <asantamv@BCN-50N7R32.usersad.everis.int>
2019-04-08 11:11:00 +0200

    Release_2

--- Syslog_Pruebas/src/Pruebas/Pruebas_Componente.java
+++ Syslog_Pruebas/src/Pruebas/Pruebas_Componente.java
@@ -1,7 +1,7 @@
 package Pruebas;
 
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 
 import beans.ConectorController;
 import beans.QueryEvento;
@@ -14,12 +14,16 @@ public class Pruebas_Componente {
 		QueryEvento evt = new QueryEvento();
 		a.addPropertyChangeListener(evt);
 
-		//Opciones del componente
+		// Opciones del componente
 		a.setDatabase("forhonor");
 		a.setHostSettings("localhost", "3306");
 		a.setCredentials("root", "");
 
-		//Consultas con usuario root
+		// Para obtener los results de un select
+		ArrayList<String> tmp = a.getResultsSelect("SELECT * from users");
+		System.out.println("Numero de filas retornadas: " + tmp.size());
+
+		// Consultas con usuario root
 		System.out.println("SELECT * FROM PERSONAJE;");
 		a.ejecutarQuery("Select * from personaje");
 		System.out.println("SELECT * FROM USERS");
@@ -27,16 +31,17 @@ public class Pruebas_Componente {
 		System.out.println("CALL test2;");
 		a.ejecutarQuery("CALL test2");
 
-		//Consultas con usuario arnau
+		// Consultas con usuario arnau
 		a.setCredentials("arnau", "");
 		System.out.println("SELECT * FROM FACCION");
 		a.ejecutarQuery("Select * from faccion");
-		//Cambiamos de base de datos pero no de usuario
+		// Cambiamos de base de datos pero no de usuario
 		a.setDatabase("bv");
 		System.out.println("SELECT * FROM BFPLAYER");
 		a.ejecutarQuery("select * from bfplayer");
 
-		//Realizamos un insert -> update -> remove (Comentar y descomentar para validar proceso)
+		// Realizamos un insert -> update -> remove (Comentar y descomentar para validar
+		// proceso)
 		System.out.println("Realizamos un insert");
 		a.ejecutarQuery(
 				"INSERT INTO `bfplayer`(`user_id`, `class_id`, `primary_weapon`, `device1`, `kills`, `Deads`) VALUES ('ARNOLD',2,1,2,9,7)");
@@ -45,7 +50,7 @@ public class Pruebas_Componente {
 		System.out.println("Ejecutamos remove");
 		a.ejecutarQuery("delete from bfplayer where user_id = 'ARNOLD_PRO'");
 
-		//Imprimimos reportes de las sentencias ejecutadas
+		// Imprimimos reportes de las sentencias ejecutadas
 		System.out.println("\n\n\n\nPrintado de reporte de bbdd forhonor con query select");
 		a.printReport("forhonor", TransactionType.SELECT);
 
